8e339bf4098f539d05c5554cc49a3cefa65db218
linabeauty <cabamiyo@gmail.com>
2023-02-18 10:33:59 -0800

    Hello put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * maim - this is the main assembling function
+ * main - Entry point
  *
- * Return: always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
-(	
+{
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
-)	
+}
