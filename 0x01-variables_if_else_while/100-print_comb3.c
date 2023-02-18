0fde1c1e881e534dbc33ac6631e479aba02a788a
linabeauty <cabamiyo@gmail.com>
2023-02-18 04:52:27 -0800

    Combination of brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - program that prints all possible different combinations of two digits
+ * main - prints all possible different combinations of two digits
  * in ascending order, separated by a comma followed by a space.
  *
- * Return: Always 0.
+ * Return: Always o.
  *
  */
 
@@ -16,11 +16,14 @@ int main(void)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 		{
-		putchar((digit1 % 10) + '0');
-		putchar((digit2 % 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-		if (digit1 == 8 && digit2 == 9)
-		continue;
+			if (digit1 == 8 && digit2 == 9)
+			continue;
+
+			putchar(',');
+			putchar(',');
 		}
 	}
 	putchar('\n');
