e4d40ceb31fc6b375e5ce301e507f4b92c0bf05b
linabeauty <cabamiyo@gmail.com>
2023-02-18 22:15:18 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
-/** main - this function print the string inside quotations
- *
- * Return: 0 when successful
+/**
+ * main - A program that prints a line using the printf function
+ * Return: 0 (success)
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, nut theoutcome is a piece of art,\n");
 	return (0);
-}	
+}
