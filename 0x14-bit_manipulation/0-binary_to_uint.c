2845d8f718f9aca5e76a705ca101797c3636a744
linabeauty <cabamiyo@gmail.com>
2023-07-07 09:36:22 -0700

    Update File

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,22 +18,20 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 
 	ui = 0;
-	len = 0;
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-	while (b[len] != '\0')
-		len++;
-
-	base_two = 1;
-
-	for (ui = len - 1; ui <= 0; ui--)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[ui] != '0' && b[ui] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-
-		if (b[ui] == '1')
+		}
+		if (b[len] & 1)
+		{
 			ui += base_two;
 
-		base_two *= 2;
+		}
 	}
 	return (ui);
 }
